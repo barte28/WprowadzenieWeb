cd42709ee935f94a7b3946f34f07f8fdd2bcdb8f
barte28 <bartek.teuerle@gmail.com>
2024-11-14 08:43:15 +0100

    rozmieszczenie grid

--- WpfApp2/obj/Debug/net6.0-windows/Window4.g.i.cs
+++ WpfApp2/obj/Debug/net6.0-windows/Window4.g.i.cs
@@ -53,7 +53,7 @@ namespace WpfApp2 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp2;component/window4.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp2;V1.0.0.0;component/window4.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Window4.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
